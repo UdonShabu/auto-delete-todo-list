cb1cd1e5d20d60f279224852b6efd57740805c87
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2025-05-26 07:02:52 +0700

    chore: update public port and host

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,4 +11,9 @@ fastify.listen({ port: 3000 }, (err, address) => {
   console.log(`🚀 Server listening at ${address}`);
 });
 
+fastify.listen({
+  port: process.env.PORT ? Number(process.env.PORT) : 3000,
+  host: "0.0.0.0",
+});
+
 export default fastify;
